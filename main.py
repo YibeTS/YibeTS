81166f5e601f737f8f0189cb2e13a5ccc970fc7b
YibeTS <yibeetorfs@gmail.com>
2024-06-09 14:48:13 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+def random_number():
+    return random.randint(1, 10)
+def game():
+    input('***NUMBER 15*** \n(made by Yibe) \n \npress enter to play: \n')
+    for i in range(1):
+      number = random_number()
+      guess = int(input('guess number: \n'))
+      if guess == number:
+        print('won! \n \n')
+        game()
+      else:
+        print('lose! \nthe number was: ', number, '\n \n')
+        game()
+game()
